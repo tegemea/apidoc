1b144dfff8defddb8abb84f015485883654035e8
lema <lemajunior@hotmail.com>
2022-01-29 17:54:53 +0300

    just added semicolons

--- src/main.js
+++ src/main.js
@@ -1,38 +1,38 @@
-import Vue from 'vue'
-import App from './App.vue'
-import 'bootstrap'
+import Vue from 'vue';
+import App from './App.vue';
+import 'bootstrap';
 import VueSweetalert2 from 'vue-sweetalert2';
 import VueRouter from 'vue-router';
-import Axios from 'axios'
+import Axios from 'axios';
 import Vuelidate from 'vuelidate';
-import jQuery from 'jquery'
-import Lodash from 'lodash'
+import jQuery from 'jquery';
+import Lodash from 'lodash';
 
 
 import 'sweetalert2/dist/sweetalert2.min.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 Vue.prototype.$axios = Axios;
 Vue.prototype.$jquery = jQuery;
 Vue.prototype.$_ = Lodash;
 
 // basic global variables
-Vue.prototype.$baseServerURL = 'http://192.168.1.2'
-Vue.prototype.$apiURL = `http://192.168.1.2/api_doc`
+Vue.prototype.$baseServerURL = 'http://192.168.1.2';
+Vue.prototype.$apiURL = 'http://192.168.1.2/api_doc';
 
 // import components for use in routes
-import Dashboard from '@/components/Dashboard'
-import Applications from '@/components/Applications'
-import Modules from '@/components/Modules'
-import Terminals from '@/components/Terminals'
-import TerminalParameters from '@/components/TerminalParameters'
-import HttpCodes from '@/components/HttpCodes'
-import TerminalExamples from '@/components/TerminalExamples'
-import Tables from '@/components/Tables'
-import TableFields from '@/components/TableFields'
+import Dashboard from '@/components/Dashboard';
+import Applications from '@/components/Applications';
+import Modules from '@/components/Modules';
+import Terminals from '@/components/Terminals';
+import TerminalParameters from '@/components/TerminalParameters';
+import HttpCodes from '@/components/HttpCodes';
+import TerminalExamples from '@/components/TerminalExamples';
+import Tables from '@/components/Tables';
+import TableFields from '@/components/TableFields';
 
 const routes = [
   { path:'/', redirect: '/dashboard' },
