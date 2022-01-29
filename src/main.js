4b21a5c49837849479f8cb838c699ccfa391c408
lema <lemajunior@hotmail.com>
2022-01-29 17:49:52 +0300

    changed server ip

--- src/main.js
+++ src/main.js
@@ -20,8 +20,8 @@ Vue.prototype.$jquery = jQuery;
 Vue.prototype.$_ = Lodash;
 
 // basic global variables
-Vue.prototype.$baseServerURL = 'http://192.168.1.6'
-Vue.prototype.$apiURL = `http://192.168.1.6/api_doc`
+Vue.prototype.$baseServerURL = 'http://192.168.1.2'
+Vue.prototype.$apiURL = `http://192.168.1.2/api_doc`
 
 // import components for use in routes
 import Dashboard from '@/components/Dashboard'
